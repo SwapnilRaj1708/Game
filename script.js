8d8af5723030328f5ffcbf509a4d0895135793c7
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2021-10-11 18:00:36 +0530

    Background Created

---
+++ script.js
@@ -0,0 +1,12 @@
+function start(){
+    document.getElementById("start_screen").style.display="none";
+    // document.body.style.backgroundColor="brown";
+    document.getElementById("goal_background").style.display="block";
+    setTimeout(function(){
+        document.getElementById("goal_background").style.display="none";
+    },1000);
+    setTimeout(function(){
+        document.getElementById("level1").style.display="block";
+    },3501)
+    
+}
