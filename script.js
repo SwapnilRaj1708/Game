74bac9ca18d3103dc5c6d462a26d18579b5d885f
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2021-10-12 12:05:41 +0530

    Added Gold Images

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var rotation=-75;
+function rotate_box(){
+    rotation +=5;
+    if(rotation > 75){
+        rotation=-75;
+    }
+    document.getElementById("box").style.transform= "rotate("+rotation+"deg)";
+    rotate_box();
+}
 function start(){
     document.getElementById("start_screen").style.display="none";
     // document.body.style.backgroundColor="brown";
@@ -7,6 +16,12 @@ function start(){
     },1000);
     setTimeout(function(){
         document.getElementById("ground").style.display="block";
-    },3501)
-    
+        document.getElementById("b_gold1").style.display="block";
+        document.getElementById("b_gold2").style.display="block";
+        document.getElementById("s_gold1").style.display="block";
+        document.getElementById("s_gold2").style.display="block";
+        document.getElementById("s_gold3").style.display="block";
+        document.getElementById("s_gold4").style.display="block";
+        document.querySelector("#box").style.display="block";
+    },1000)
 }
